5d5d22d818908e0297e61f41746eb0c78789717d
Aziz <azizekeathn@gmail.com>
2021-04-16 18:51:49 +0300

    day4

--- src/streamOrnekler/Stream04String.java
+++ src/streamOrnekler/Stream04String.java
@@ -1,6 +1,7 @@
 package streamOrnekler;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Stream04String {
@@ -13,13 +14,27 @@ public class Stream04String {
         liste.add("Jackson");
         liste.add("Amanda");
         liste.add("Mariano");
+        liste.add("Alihano");
+        liste.add("Alonzo");
         liste.add("Alberto");
+        liste.add("Alfanso");
         liste.add("Tucker");
         liste.add("Christ");
+
 
         aIleBaslayanlar(liste);
         System.out.println("===========");
         buyukHarfeCevir(liste);
+        System.out.println("===========");
+        uzunlugaGoreYazdir(liste,3);
+        System.out.println("TÜM ELEMANLAR BELİRTİLEN DEĞERDEN KÜÇÜK:" + uzunlukKucukMu(liste,7));
+        System.out.println("BAŞLAYAN YOK MU:"+ baslamayanHarfVarMi(liste,"M"));
+        System.out.println("HERHANGİ BİTEN HARF VAR MI:" + herhangiBitenVarMi(liste,"c"));
+        System.out.println("===========");
+        listeleYazdir(liste);
+        System.out.println("===========");
+        yazdir(liste);
+
     }
 
     //********************************************************************************************
@@ -36,6 +51,64 @@ public class Stream04String {
         liste.stream().map(h-> h.toUpperCase()).forEach(System.out::println);
     }
 
+    //********************************************************************************************
+    // ORNEK18: Listedeki tüm elemanların uzunlukları belirtilen uzunluktan fazla ise bunları yazdıran
+    // metodu tanımlayalım..
+    //********************************************************************************************
+    public static void uzunlugaGoreYazdir (List <String> liste, int uzunluk){
+        liste.stream().filter(t->t.length() > uzunluk).forEach(System.out::println);
+    }
 
+    //*****************************************************************************************
+    // ÖRNEK20: Listedeki TÜM elemanların uzunlukları belirtilen uzunluktan KÜÇÜK mü
+    // diye kontrol eden metodu yazınız.
+    //*****************************************************************************************
+    //ALLMATCH() Belirtilen şartları tüm elemanlar sağlıyorsa true döndürür. yoksa false
+    public static boolean uzunlukKucukMu(List <String> liste,int uzunluk ){
+        return liste.stream().allMatch(t->t.length() < uzunluk);
+    }
+    //****************************************************************************************************
+    // ÖRNEK21: Listedeki TÜM elemanların belirtilen harfi ile başlamadığını kontrol eden metodu yazınız.
+    //****************************************************************************************************
+    //NONEMATCH() Belirtilen sartları tüm elemanlar sağlamıyorsa true döndürür. yoksa false
+    public static boolean baslamayanHarfVarMi(List <String> liste, String harf){
+        return liste.stream().noneMatch(t->t.startsWith(harf));
+    }
+
+    //****************************************************************************************************
+    // ÖRNEK22: Listede Herhangi bir elaman belirtilen bir harf ile bitiyor mu diye kontrol eden metodu yazınız.
+    //****************************************************************************************************
+    public static boolean herhangiBitenVarMi(List <String> liste, String harf){
+        return liste.stream().anyMatch(t->t.endsWith(harf));
+    }
+
+    //****************************************************************************************************
+    // ÖRNEK23:A ile baslayip O ile biten elemalri yazdiran metodu yaziran.
+    //****************************************************************************************************
+
+    public static void aileOYazdir(List<String> liste){
+        liste.stream().filter(x->x.startsWith("A") && x.endsWith("o")).forEach(System.out::println);
+
+
+    }
+
+        //****************************************************************************************************
+    //  ÖRNEK24: Aşağıdaki formata göre listedeki her bir elemanın uzunluğunu yazdıran metodu yazınız.
+    //  Ali: 3        Mark: 4       Amanda: 6     vb.
+    //****************************************************************************************************
+
+    public static void listeleYazdir(List<String>liste){
+        liste.stream().
+                sorted(Comparator.comparing(String::length)).
+                map(t->t + " : "+ t.length()+ "\t").
+                forEach(System.out::println);
+    }
+
+    public static void yazdir(List <String>liste) {
+        liste.stream().
+                map(String::toLowerCase).
+                filter(t->t.startsWith("a")).
+                forEach(System.out::println);
+    }
 
 }
